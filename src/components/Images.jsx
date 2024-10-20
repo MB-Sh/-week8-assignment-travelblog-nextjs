028b2f4db4c0fd98a2c1ea3a584f61e9d85c0e7d
Mary Sharp <marysharp0721@gmail.com>
2024-10-20 14:49:38 +0100

    added image component

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,33 +1,18 @@
-// "use client";
-// import Image from "next/image";
-// import ballon from "@/../public/ballon.jpg";
-// import car from "@/../public/car.jpg";
-// import hamburg from "@/../public/hamburg.jpg";
-// import train from "@/../public/train.jpg";
-// import camera from "@/../public/camera.jpg";
-// import ship from "@/../public/ship.jpg";
-// import { motion } from "framer-motion";
-import Images from "@/components/Images";
+ "use client";
+import Image from "next/image";
+import ballon from "@/../public/ballon.jpg";
+import car from "@/../public/car.jpg";
+import hamburg from "@/../public/hamburg.jpg";
+import train from "@/../public/train.jpg";
+import camera from "@/../public/camera.jpg";
+import ship from "@/../public/ship.jpg";
+import { motion } from "framer-motion";
 
 
+export default function Images() {
 
-
-
-export const metadata = {
-    title: "Home page",
-    description: "This blogs is for people who love to travel",
-  };
-  
-export default function HomePage() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <h2 className="text-center text-4xl font-bold text-gray-800 my-10">Explore Our Travel Moments</h2>
-      <p className="text-center text-lg mb-10 text-gray-600">
-        Discover stunning destinations and moments captured by travelers around the world.
-      </p>
-
-      <Images/>
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6">
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6">
         
         <motion.div
           whileHover={{ scale: 1.05 }}
@@ -141,8 +126,8 @@ export default function HomePage() {
             className="rounded-lg object-cover"
             placeholder="blur"
           />
-        </motion.div> */}
-      {/* </div> */}
-    </div>
-  );
-}
+        </motion.div>
+      </div>
+        
+      );
+    }
